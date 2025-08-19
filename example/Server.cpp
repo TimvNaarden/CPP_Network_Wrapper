6fbe8885af81f1fd139e8bfd6f643906cb1c9ebf
Tim van Naarden <TimvNaarden@outlook.com>
2025-08-19 15:41:39 +0200

    Added example

---
+++ example/Server.cpp
@@ -0,0 +1,15 @@
+#include "include/TCP/TCPServer.h"
+char Ip[] = "0.0.0.0";
+
+void TCP_Server_Loop(Networking::TCPServer *server, SOCKET sock, SSL *ssl) {
+  std::cout << "Client connected" << std::endl;
+
+  char *Message = (char *)"Hello";
+  server->Send(sock, Message);
+}
+
+int main() {
+  Networking::TCPServer MyServer(Networking::IPV4, 7864, Ip, 0);
+  MyServer.StartListening(TCP_Server_Loop);
+  return 0;
+}
