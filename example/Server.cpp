46bb0d1cf7c7e7f788a448c11f1fc41308275e74
Tim <TimvNaarden@outlook.com>
2025-08-25 16:04:51 +0200

    Update Server.cpp

--- example/Server.cpp
+++ example/Server.cpp
@@ -1,10 +1,19 @@
 #include "include/TCP/TCPServer.h"
-#include <thread>
+#define _POSIX_C_SOURCE 199309L
+#include <time.h>
+
+void sleep_us(unsigned long microseconds) {
+    struct timespec ts;
+    ts.tv_sec = microseconds / 1000000ul;            // whole seconds
+    ts.tv_nsec = (microseconds % 1000000ul) * 1000;  // remainder, in nanoseconds
+    nanosleep(&ts, NULL);
+}
+
 char Ip[] = "0.0.0.0";
 
 void TCP_Server_Loop(Networking::TCPServer *server, SOCKET sock, SSL *ssl) {
   std::cout << "Client connected" << std::endl;
-  std::this_thread::sleep_for(2000ms);
+  sleep(100000);
   char *Message = (char *)"Hello";
   server->Send(sock, Message);
   std::cout << "Message send" << std::endl;
