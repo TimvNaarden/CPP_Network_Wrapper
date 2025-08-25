b079d243d8441acab8288a63c0ddd4ab603ad7d3
Tim <TimvNaarden@outlook.com>
2025-08-25 15:35:15 +0200

    Update Server.cpp

--- example/Server.cpp
+++ example/Server.cpp
@@ -1,9 +1,17 @@
 #include "include/TCP/TCPServer.h"
+
+#ifdef __WINDOWS__
+#include <windows.h>
+#endif
+
+#ifdef __LINUX__
+#include <unistd.h>
+#endif
 char Ip[] = "0.0.0.0";
 
 void TCP_Server_Loop(Networking::TCPServer *server, SOCKET sock, SSL *ssl) {
   std::cout << "Client connected" << std::endl;
-
+  usleep(100000);  
   char *Message = (char *)"Hello";
   server->Send(sock, Message);
   std::cout << "Message send" << std::endl;
