343eedd41e5f098735c1dfd9a65cd7143ba3c0f4
Tim van Naarden <TimvNaarden@outlook.com>
2025-08-25 15:52:39 +0200

    Example

--- example/Server.cpp
+++ example/Server.cpp
@@ -4,14 +4,14 @@
 #include <windows.h>
 #endif
 
-#ifdef __linux__
+#ifndef __win32
 #include <unistd.h>
 #endif
 char Ip[] = "0.0.0.0";
 
 void TCP_Server_Loop(Networking::TCPServer *server, SOCKET sock, SSL *ssl) {
   std::cout << "Client connected" << std::endl;
-  usleep(100000);  
+  usleep(100000);
   char *Message = (char *)"Hello";
   server->Send(sock, Message);
   std::cout << "Message send" << std::endl;
