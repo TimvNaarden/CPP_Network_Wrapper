b43209a9f4220870ca3950da36f715acb9c02598
Tim van Naarden <TimvNaarden@outlook.com>
2025-08-25 22:22:36 +0200

    Example

--- include/TCP/TCPClient.h
+++ include/TCP/TCPClient.h
@@ -62,7 +62,7 @@ public:
    * @param SSL Enable SSL (default 0)
    */
   TCPClient(iProtocol iProt, UINT16 port, char *ip, int SSL = 0);
-  ~TCPClient() {};
+  ~TCPClient();
 
   /**
    * @brief send data to the server
