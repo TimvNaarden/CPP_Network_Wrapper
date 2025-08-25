df1db112968b2b108c1f4d7c046eaf8604f91929
Tim van Naarden <TimvNaarden@outlook.com>
2025-08-25 23:53:06 +0200

    Fixed UDP packet size transfer

--- include/UDP/UDPServer.cpp
+++ include/UDP/UDPServer.cpp
@@ -1,86 +1,96 @@
 #include "UDPServer.h"
 
 namespace Networking {
-	int g_UDPServerCount = 0;
+int g_UDPServerCount = 0;
 
-	UDPServer::UDPServer(iProtocol iProt, UINT16 port, char* ip) {
-		STARTWSA();
-		m_Socket = socket(iProt, SOCK_DGRAM, IPPROTO_UDP);
-		if (m_Socket == INVALID_SOCKET) {
-			std::cerr <<"Failed to create socket" << std::endl;
-			return;
-		}
-		g_UDPServerCount++;
+UDPServer::UDPServer(iProtocol iProt, UINT16 port, char *ip) {
+  STARTWSA();
+  m_Socket = socket(iProt, SOCK_DGRAM, IPPROTO_UDP);
+  if (m_Socket == INVALID_SOCKET) {
+    std::cerr << "Failed to create socket" << std::endl;
+    return;
+  }
+  g_UDPServerCount++;
 
-		sockaddr_in ServerAddress{};
-		ServerAddress.sin_family = iProt;
-		ServerAddress.sin_port = htons(port);
-		ServerAddress.sin_addr.s_addr = inet_addr(ip);
+  sockaddr_in ServerAddress{};
+  ServerAddress.sin_family = iProt;
+  ServerAddress.sin_port = htons(port);
+  ServerAddress.sin_addr.s_addr = inet_addr(ip);
 
-		if (bind(m_Socket, (sockaddr*)&ServerAddress, sizeof(ServerAddress)) == -1) {
-			std::cerr <<"Failed to bind socket" << std::endl;
-			return;
-		}
+  if (bind(m_Socket, (sockaddr *)&ServerAddress, sizeof(ServerAddress)) == -1) {
+    std::cerr << "Failed to bind socket" << std::endl;
+    return;
+  }
 
-		m_iProt = iProt;
-	}
-
-	UDPServer::~UDPServer() {
-		g_UDPServerCount--;
-		if (g_UDPServerCount == 0) {
-			WSACleanup();
-		}
-	}
-
-	int UDPServer::Send(const char* data, char* ip, UINT16 port, int length) {
-		sockaddr_in ServerAddress{};
-		ServerAddress.sin_family = m_iProt;
-		ServerAddress.sin_port = htons(port);
-		ServerAddress.sin_addr.s_addr = inet_addr(ip);
-
-		size_t size = (length) ? length : strlen(data);
-		std::string SizePacket = std::to_string(size);
-
-		int ResultSize = sendto(m_Socket, SizePacket.c_str(), SizePacket.size(), 0, (sockaddr*)&ServerAddress, sizeof(ServerAddress));
-		if (ResultSize == -1) {
-			std::cerr <<"Failed to send size packet" << std::endl;
-			return 1;
-		}
-		int TotalSent = 0;
-		while (TotalSent < size) {
-			int Sent = sendto(m_Socket, data + TotalSent, size - TotalSent, 0, (sockaddr*)&ServerAddress, sizeof(ServerAddress));
-			if (Sent == -1) {
-				std::cerr <<"Failed to send message" << std::endl;
-				return 1;
-			}
-			TotalSent += Sent;
-		}
-
-		return 0;
-	}
-
-	int UDPServer::Receive(char*& data, sockaddr_in* ServerAddress) {
-		socklen_t ServerAddressLength = sizeof(*ServerAddress);
-
-		char SizePacket[20]; // 64-bit integer can be at most 20 characters long
-		int ResultSize = recvfrom(m_Socket, (RECVFROM_BUFFER)SizePacket, 19, 0, (sockaddr*)&ServerAddress, &ServerAddressLength);
-		if (ResultSize == -1) {
-			std::cerr <<"Failed to receive data" << std::endl;
-			return 1;
-		}
-		SizePacket[ResultSize] = '\0';
-
-		data = new char[std::stoi(SizePacket) + 1];
-		int TotalReceived = 0;
-		while (TotalReceived < std::stoi(SizePacket)) {
-			int Received = recvfrom(m_Socket, (RECVFROM_BUFFER)data + TotalReceived, std::stoi(SizePacket) - TotalReceived, 0, (sockaddr*)&ServerAddress, &ServerAddressLength);
-			if (Received == -1) {
-				std::cerr <<"Failed to receive data" << std::endl;
-				return 1;
-			}
-			TotalReceived += Received;
-		}
-		data[TotalReceived] = '\0';
-		return 0;
-	}
+  m_iProt = iProt;
 }
+
+UDPServer::~UDPServer() {
+  g_UDPServerCount--;
+  if (g_UDPServerCount == 0) {
+    WSACleanup();
+  }
+}
+
+int UDPServer::Send(const char *data, char *ip, UINT16 port, int length) {
+  sockaddr_in ServerAddress{};
+  ServerAddress.sin_family = m_iProt;
+  ServerAddress.sin_port = htons(port);
+  ServerAddress.sin_addr.s_addr = inet_addr(ip);
+
+  size_t size = (length) ? length : strlen(data);
+  std::string SizePacket = std::to_string(size) + "\n";
+
+  int ResultSize = sendto(m_Socket, SizePacket.c_str(), SizePacket.size(), 0,
+                          (sockaddr *)&ServerAddress, sizeof(ServerAddress));
+  if (ResultSize == -1) {
+    std::cerr << "Failed to send size packet" << std::endl;
+    return 1;
+  }
+  int TotalSent = 0;
+  while (TotalSent < size) {
+    int Sent = sendto(m_Socket, data + TotalSent, size - TotalSent, 0,
+                      (sockaddr *)&ServerAddress, sizeof(ServerAddress));
+    if (Sent == -1) {
+      std::cerr << "Failed to send message" << std::endl;
+      return 1;
+    }
+    TotalSent += Sent;
+  }
+
+  return 0;
+}
+
+int UDPServer::Receive(char *&data, sockaddr_in *ServerAddress) {
+  socklen_t ServerAddressLength = sizeof(*ServerAddress);
+
+  char SizePacket[20]; // 64-bit integer can be at most 20 characters long
+  for (int pos = 0;; pos++) {
+    int ResultSize = recvfrom(m_Socket, (RECVFORM_BUFFER)SizePacket, 1, 0,
+                              (sockaddr *)ServerAddress, &ServerAddressLength);
+    if (ResultSize <= 0) {
+      std::cerr << "Failed to receive size from client" << std::endl;
+      return -1;
+    }
+    if (SizePacket[pos] == '\n') {
+      SizePacket[pos] = '\0';
+      break;
+    }
+  }
+
+  data = new char[std::stoi(SizePacket) + 1];
+  int TotalReceived = 0;
+  while (TotalReceived < std::stoi(SizePacket)) {
+    int Received = recvfrom(m_Socket, (RECVFROM_BUFFER)data + TotalReceived,
+                            std::stoi(SizePacket) - TotalReceived, 0,
+                            (sockaddr *)&ServerAddress, &ServerAddressLength);
+    if (Received == -1) {
+      std::cerr << "Failed to receive data" << std::endl;
+      return 1;
+    }
+    TotalReceived += Received;
+  }
+  data[TotalReceived] = '\0';
+  return 0;
+}
+} // namespace Networking
